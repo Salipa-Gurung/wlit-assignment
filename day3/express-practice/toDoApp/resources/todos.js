10e48ab09f032941a49e7f67f31b2472254dd841
Salipa-Gurung <salipagurung@gmail.com>
2022-03-08 09:11:13 +0545

    implementing crud in express day 5

---
+++ day3/express-practice/toDoApp/resources/todos.js
@@ -0,0 +1,14 @@
+const todos = [
+    {
+    id: '001',
+    name: 'Buy groceries',
+    description: 'potato, slipper',
+    },
+    {
+        id: '002',
+        name: 'complete bootcamp project',
+        description: 'complete bootcamp project',
+    }
+]
+
+module.exports = todos;
--- day3/express-practice/toDoApp/routes/index.js
+++ day3/express-practice/toDoApp/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 var router = express.Router();
+
+const todos = require('../resources/todos');  //importing todos module
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +11,38 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('homepage');
+  res.render('homepage', {todoList: todos});
+});
+
+router.get('/add-todo', function(req, res, next){
+  res.render('addTodo');
+});
+
+router.post('/save-todo', function(req, res, next) {
+  console.log(req.body);
+
+  todos.push({...req.body, id:`00${ todos.length + 1}`})
+  res.redirect('/home');
+});
+
+router.get('/edit-todo/:id', function(req, res, next){
+  console.log('edited');
+  const todo = todos.find(todo => todo.id === req.params.id)
+  res.render('editTodo', {todo: todo});
+});
+
+router.post('/save-updated/:id', function(req, res, next){
+  console.log('save update');
+  const index = todos.findIndex(todo => todo.id === req.params.id);
+  todos.splice(index, 1, {...req.body, id: index});
+  res.redirect('/home');
+});
+
+router.get('/delete-todo/:id', function(req, res, next) {
+console.log(req.params.id);
+  const index = todos.findIndex(todo => todo.id === req.params.id)
+  todos.splice(index, 1)
+  res.redirect('/home')
 });
 
 module.exports = router;
